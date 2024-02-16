72a457a397040a77330ce5576ddabec9c7525eb7
BT_2N2_15 <sample@bths.edu>
2024-02-16 13:58:14 -0500

    fixes

--- src/Movie.java
+++ src/Movie.java
@@ -5,67 +5,45 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 public class Movie {
-   private ArrayList<String> titles;
-   private ArrayList<String> casts;
-   private ArrayList<String> directors;
-   private ArrayList<String> overviews;
-   private ArrayList<Integer> runtimes;
-   private ArrayList<Double> ratings;
+   private String title;
+   private String cast;
+   private String director;
+   private String overview;
+   private int runtime;
+   private double rating;
    private Scanner scan;
 
-   public Movie() {
+   public Movie(String title, String cast, String director, String overview, int runtime, double rating) {
       ArrayList<String> temp = new ArrayList<>();
-      titles = new ArrayList<>();
-      casts = new ArrayList<>();
-      directors = new ArrayList<>();
-      overviews = new ArrayList<>();
-      runtimes = new ArrayList<>();
-      ratings = new ArrayList<>();
-      try {
-         File myFile = new File("src\\movies_data.csv");
-         Scanner fileScanner = new Scanner(myFile);
-         while (fileScanner.hasNext()) {
-            String data = fileScanner.nextLine();
-            String[] splitData = data.split(",");
-            titles.add(splitData[0]);
-            casts.add(splitData[1]);
-            directors.add(splitData[2]);
-            overviews.add(splitData[3]);
-            runtimes.add(Integer.parseInt(splitData[4]));
-            ratings.add(Double.parseDouble(splitData[5]));
-         }
-         titles.remove(0);
-         casts.remove(0);
-         directors.remove(0);
-         overviews.remove(0);
-         runtimes.remove(0);
-         ratings.remove(0);
-      } catch (IOException exception) {
-         System.out.println(exception.getMessage());
-      }
+      this.title = title;
+      this.cast = cast;
+      this.director = director;
+      this.overview = overview;
+      this.runtime = runtime;
+      this.rating = rating;
    }
 
-   public ArrayList<String> getTitles() {
-      return titles;
+   public String getTitles() {
+      return title;
    }
 
-   public ArrayList<String> getCasts() {
-      return casts;
+   public String getCasts() {
+      return cast;
    }
 
-   public ArrayList<String> getDirectors() {
-      return directors;
+   public String getDirectors() {
+      return director;
    }
 
-   public ArrayList<String> getOverviews() {
-      return overviews;
+   public String getOverviews() {
+      return overview;
    }
 
-   public ArrayList<Integer> getRuntimes() {
-      return runtimes;
+   public int getRuntimes() {
+      return runtime;
    }
 
-   public ArrayList<Double> getRatings() {
-      return ratings;
+   public double getRatings() {
+      return rating;
    }
 }
--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,17 +1,35 @@
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 public class MovieCollection {
     private Scanner scan;
-    private Movie movie;
+    private ArrayList<Movie> movies;
 
     public MovieCollection() {
-        movie = new Movie();
+        movies = new ArrayList<>();
         start();
     }
 
     private void start() {
+        try {
+            File myFile = new File("src\\movies_data.csv");
+            Scanner fileScanner = new Scanner(myFile);
+            int count = 0;
+            while (fileScanner.hasNext()) {
+                if (count > 0) {
+                    String data = fileScanner.nextLine();
+                    String[] splitData = data.split(",");
+                    Movie movie = new Movie(splitData[0], splitData[1], splitData[2], splitData[3], Integer.parseInt(splitData[4]), Double.parseDouble(splitData[5]));
+                    movies.add(movie);
+                }
+                count++;
+            }
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
         scan = new Scanner(System.in);
         System.out.println("Welcome to the movie collection!");
         String menuOption = "";
@@ -38,7 +56,42 @@ public class MovieCollection {
     }
 
     private void searchTitles() {
+        System.out.println("Enter title term: ");
+        String title = scan.nextLine();
+        ArrayList<String> listOfTitles = new ArrayList<>();
+        ArrayList<String> titles = movie.getTitles();
+        for (int i = 0; i < titles.size(); i++) {
+            if (titles.get(i).toLowerCase().contains(title.toLowerCase())) {
+                listOfTitles.add(titles.get(i));
+            }
+        }
+        for (int i = 0; i < listOfTitles.size(); i++) {
+            int index = i;
+            String min = listOfTitles.get(i);
+            for (int j = i + 1; j < listOfTitles.size(); j++) {
+                if (listOfTitles.get(j).compareTo(min) < 0) {
+                    min = listOfTitles.get(j);
+                    index = j;
+                }
+            }
+            String temp = listOfTitles.get(i);
+            listOfTitles.set(i, min);
+            listOfTitles.set(index, temp);
+        }
+        for (int i = 0; i < listOfTitles.size(); i++) {
+            System.out.println(i + 1 + " " + listOfTitles.get(i));
+        }
 
+        System.out.println("Which movie would you like to know more about?");
+        int option = scan.nextInt();
+        int index = titles.indexOf(listOfTitles.get(option));
+        scan.nextLine();
+        System.out.println();
+        System.out.println("Title: " + listOfTitles.get(index));
+        System.out.println("Runtime: " + movie.getRuntimes().get(index));
+        System.out.println("Directed by: " + movie.getDirectors().get(index));
+        System.out.println("Cast: " + movie.getCasts().get(index));
+        System.out.println("Overview: " + movie.getOverviews().get(index));
     }
 
     private void searchCast() {
